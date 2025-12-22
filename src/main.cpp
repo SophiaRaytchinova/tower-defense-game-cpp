1750e571854904a1fe3b82ffd115b65500b2df6e
SophiaRaytchinova <sraychinova@gmail.com>
2025-12-22 23:55:21 +0200

    defining symbols for empty ground, path/wall, enemy and base and setting the enemies position

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ int main(){
 
     const char* map[ROWS][COLS];
 
+    const char emptyGround='.';
+    const char pathWall='#';
+    const char enemy='E';
+    const char base='B';
+
+    int enemyX=1;
+    int enemyY=1;
+
     return 0;
 }
 
