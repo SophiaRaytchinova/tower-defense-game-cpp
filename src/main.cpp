dc418c09240b89ede940b3dfaf1c2fac69d40d40
SophiaRaytchinova <sraychinova@gmail.com>
2025-12-23 00:24:13 +0200

    adding enemies hardcoded path, based on the already set grid

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,11 @@ using std::endl;
 const int ROWS=10;
 const int COLS=10;
 
+std::vector<std::pair<int, int>> path = {
+        {1,1}, {1,2}, {2,2}, {3,2}, {3,3}, {3,4}, {3,5}, {3,6},
+        {4,6}, {5,6}, {6,6}, {7,6}, {8,6}, {8,7}, {8,8}, {8,9}
+    };
+
 int main(){
 
     const char* map[ROWS][COLS]={
