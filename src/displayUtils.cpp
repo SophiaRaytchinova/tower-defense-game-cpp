96d8c41a571636c654e3183dd3c55576fc954cd3
SophiaRaytchinova <sraychinova@gmail.com>
2026-02-25 10:02:19 +0200

    updated tower color to be yellow

--- src/displayUtils.cpp
+++ src/displayUtils.cpp
@@ -12,7 +12,7 @@ using std::endl;
 
 const char* RED   = "\x1b[1;31m";
 const char* BLUE  = "\x1b[1;34m";
-const char* GREEN = "\x1b[38;5;82m";
+const char* YELLOW = "\x1b[1;33m";
 const char* RESET = "\x1b[0m";
 
 const char emptyGround = '.';
@@ -35,7 +35,7 @@ void render(char map[ROWS][COLS], std::vector<Enemy>& enemies) {
                 buffer << BLUE << map[i][j] << RESET << " ";
             }
             else if (map[i][j] == 'T') {
-                buffer << GREEN << map[i][j] << RESET << " ";
+                buffer << YELLOW << map[i][j] << RESET << " ";
             }
             else buffer << map[i][j] << " ";
         }
